3e291223b49e32d7f952b739a193ca6af75780e8
Tomas <tj4305@gmail.com>
2022-11-24 19:21:46 +0200

    Added register.php

---
+++ EndTask/register.php
@@ -0,0 +1,20 @@
+<?php
+
+$userName = $_POST["userName"];
+$email = $_POST["Email"];
+$password = $_POST["Password"];
+
+//DB Connection
+$connection = new mysqli("localhost", "root", "", "validateuser");
+if ($connection->connect_error) {
+    echo "$connection->connect_error";
+    die("Connection Failed : " . $connection->connect_error);
+} else {
+    $statement = $connection->prepare("insert into registration(userName, email, password) values(?, ?, ?)");
+    $statement->bind_param("sss", $userName, $email, $password);
+    $execvalue = $statement->execute();
+    echo $execvalue;
+    echo "Your Registration Is Succesfull!";
+    $statement->close();
+    $connection->close();
+}
